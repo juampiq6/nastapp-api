470faf1f0080035eae54fa79b371a4e087ec1f5a
Juampi Q <juampiq6@gmail.com>
2023-12-15 10:47:17 -0300

    fix: stopped using some useless field in PlaceResult

--- db/places_result.go
+++ db/places_result.go
@@ -1,18 +1,20 @@
 package db
 
 type PlaceResult struct {
-	Id                    string            `bson:"id" json:"id"`
-	DisplayName           string            `bson:"displayName" json:"displayName"`
-	AddressComponents     AddressComponents `bson:"addressComponents" json:"addressComponents"`
-	FormattedAddress      string            `bson:"formattedAddress" json:"formattedAddress"`
-	GoogleMapsUri         string            `bson:"googleMapsUri" json:"googleMapsUri"`
-	Location              LatLong           `bson:"location" json:"location"`
-	LocationGEOJson       LatLongGEOJson    `bson:"locationGEOJson" json:"locationGEOJson"`
-	ShortFormattedAddress string            `bson:"shortFormattedAddress" json:"shortFormattedAddress"`
-	WeekdayDescriptions   []string          `bson:"weekdayDescriptions" json:"weekdayDescriptions"`
-	PhoneNumber           string            `bson:"nationalPhoneNumber" json:"nationalPhoneNumber"`
-	Rating                string            `bson:"rating" json:"rating"`
-	UserRatingCount       string            `bson:"userRatingCount" json:"userRatingCount"`
+	MongoId string `bson:"_id" json:"_id"`
+	// GoogleMapsId, not useful for frontend
+	// Id                string            `bson:"id" json:"id"`
+	DisplayName       string            `bson:"displayName" json:"displayName"`
+	AddressComponents AddressComponents `bson:"addressComponents" json:"addressComponents"`
+	// FormattedAddress  string            `bson:"formattedAddress" json:"formattedAddress"`
+	GoogleMapsUri string  `bson:"googleMapsUri" json:"googleMapsUri"`
+	Location      LatLong `bson:"location" json:"location"`
+	// LocationGEOJson       LatLongGEOJson    `bson:"locationGEOJson" json:"locationGEOJson"`
+	ShortFormattedAddress string   `bson:"shortFormattedAddress" json:"shortFormattedAddress"`
+	WeekdayDescriptions   []string `bson:"weekdayDescriptions" json:"weekdayDescriptions"`
+	PhoneNumber           string   `bson:"nationalPhoneNumber" json:"nationalPhoneNumber"`
+	Rating                string   `bson:"rating" json:"rating"`
+	UserRatingCount       string   `bson:"userRatingCount" json:"userRatingCount"`
 }
 
 type AddressComponents struct {
